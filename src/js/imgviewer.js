4104670d04d5ae30d615e8b3b13be2fd9ab7cd38
HiddenProjects <64079569+XHiddenProjects@users.noreply.github.com>
2023-07-23 09:38:12 -0400

    Add files via upload

--- src/js/imgviewer.js
+++ src/js/imgviewer.js
@@ -16,6 +16,14 @@ class ImgViewer{
 			return lks;
 		}
 	}
+	mkNL(nL){
+		if(nL.match(/\n|\r|\r\n/)){
+			let line = nL.match(/\n|\r|\r\n/);
+			return nL.replace(/\n|\r|\r\n/,'<br/>');
+		}else{
+			return nL;
+		}
+	}
 	gallery(s){
 		if(typeof s!=='object'){
 			console.error('gallery[1] must be a JSON object');
@@ -52,7 +60,7 @@ class ImgViewer{
 				let img = gal[a].querySelectorAll('img');
 				for(let i=0;i<img.length;i++){
 					let d = document.createElement('div');
-						d.innerHTML = (s.desc.display&&!s.desc.viewer ? img[i].outerHTML+'<span class="ImgViewer-desc">'+(img[i].getAttribute('img-desc')!==null ? this.mkLk(img[i].getAttribute('img-desc')) : '')+'</span>' : img[i].outerHTML);
+						d.innerHTML = (s.desc.display&&!s.desc.viewer ? img[i].outerHTML+'<span class="ImgViewer-desc">'+(img[i].getAttribute('img-desc')!==null ? this.mkNL(this.mkLk(img[i].getAttribute('img-desc'))) : '')+'</span>' : img[i].outerHTML);
 						d.classList.add('view');
 						d.setAttribute('viewData',i+1);
 						gal[a].replaceChild(d,img[i]);
@@ -143,7 +151,7 @@ class ImgViewer{
 				 }
 				for(let b=0;b<img.length;b++){
 					let d = document.createElement('div');
-					d.innerHTML = (s.desc.display&&!s.desc.viewer ? img[b].outerHTML+'<span class="ImgViewer-desc">'+(img[b].getAttribute('img-desc')!==null ? this.mkLk(img[b].getAttribute('img-desc')) : '')+'</span>' : img[b].outerHTML);
+					d.innerHTML = (s.desc.display&&!s.desc.viewer ? img[b].outerHTML+'<span class="ImgViewer-desc">'+(img[b].getAttribute('img-desc')!==null ? this.mkNL(this.mkLk(img[b].getAttribute('img-desc'))) : '')+'</span>' : img[b].outerHTML);
 					d.classList.add('view');
 					d.setAttribute('viewData',b+1);
 					d.tabIndex = b+1;
@@ -151,6 +159,92 @@ class ImgViewer{
 				}
 			}
 		}	
+	}
+	card(s){
+		if(typeof s!=='object'){
+			console.error('card[1] must be a JSON object');
+			return false;
+		}
+		if(typeof s.desc.display!=='boolean'){
+			console.error('card[1].desc.display must be a boolean');
+			return false;
+		}
+		if(typeof s.desc.viewer!=='boolean'){
+			console.error('card[1].desc.viewer must be a boolean');
+			return false;
+		}
+		let gal = document.querySelectorAll('.imgViewer-container.loaded');
+		for(let a=0;a<gal.length;a++){
+			if(gal[a].getAttribute('view').toLowerCase()==='card'){
+				!s.desc.display||s.desc.viewer ? gal[a].classList.add('noCaption') : gal[a].classList.add('caption');
+				 s.desc.viewer ? '' : gal[a].classList.add('noView');
+				 let img = gal[a].querySelectorAll('img');
+				 if(img.length>1||img.length<=0){
+					 console.error('You have too many/too less images, REQUIRED MAX 1');
+					 return false;
+				 }
+				for(let b=0;b<img.length;b++){
+					let d = document.createElement('div');
+					d.innerHTML = (s.desc.display&&!s.desc.viewer ? img[b].outerHTML : img[b].outerHTML);
+					d.classList.add('view');
+					d.setAttribute('viewData',b+1);
+					d.tabIndex = b+1;
+					gal[a].replaceChild(d,img[b]);
+				}
+				for(let c=0;c<img.length;c++){
+					let append = document.createElement('span');
+						append.innerHTML = this.mkNL(this.mkLk(img[c].getAttribute('img-desc')));
+						append.classList.add('ImgViewer-desc')
+					gal[a].insertBefore(append, gal[a].children[0]);
+					
+				}
+			}
+		}	
+	}
+	video(s){
+		if(typeof s!=='object'){
+			console.error('video[1] must be a JSON object');
+			return false;
+		}
+		
+		let gal = document.querySelectorAll('.imgViewer-container.loaded');
+		for(let a=0;a<gal.length;a++){
+			if(gal[a].getAttribute('view').toLowerCase()==='video'){
+				!s.desc.display||s.desc.viewer ? gal[a].classList.add('noCaption') : gal[a].classList.add('caption');
+				 s.desc.viewer ? '' : gal[a].classList.add('noView');
+				let img = gal[a].querySelectorAll('video');
+				for(let b=0;b<img.length;b++){
+					s.poster[b]!==''&&s.poster.length>0 ? img[b].poster = s.poster[b] : '';
+					img[b].setAttribute('ondblclick','setFull(this.parentElement.querySelector(".fullscreen"),70)')
+					img[b].tabIndex = '1';
+					img[b].setAttribute('onkeydown','setFull(this.parentElement.querySelector(".fullscreen"), event);setMute(this.parentElement.querySelector(".mute"),event);actVid(this.parentElement.querySelector(".play-pause-btn"), event);addSec(this, '+(typeof s.skipRate!=='undefined' ? (s.skipRate.length==0||s.skipRate[b]==0 ? 5 : s.skipRate[b]) : 5)+', event);hideContent(this);');
+					img[b].setAttribute('onclick','actVid(this.parentElement.querySelector(".play-pause-btn"), 32);hideContent(this);');
+					img[b].setAttribute('oncontextmenu','showContent(this, event);return false;');
+					let d = document.createElement('div');
+						d.innerHTML = (s.desc.display&&!s.desc.viewer ? img[b].outerHTML : img[b].outerHTML);
+						d.innerHTML += (!s.desc.viewer ? '<div class="loading-container"><div class="loading-circle"></div></div><div class="video-player" onclick="hideContent(this);" tabindex="0">'+
+						'<div class="video-player-controls">'+
+						'<div class="video-progress">'+
+						'<div class="video-progress-filled"'+(s.progressFill.length>0 ? 'style="background-color:'+s.progressFill[b]+'"' : '')+'></div>'+
+						'</div>'+
+						'<span class="play-pause-btn" vid-state="paused" onclick="actVid(this, 32);"><i class="fa-solid fa-play"></i></span>'+
+						'<input class="volume" type="range" oninput="changeVidVol(this)" min="0" max="1" step="0.01" value="1"/></div>'+
+						'<div class="mute" toggle-stat="normal" onclick="setMute(this, 77)"><i class="fas fa-volume"></i></div>'+
+						'<div class="fullscreen" toggle-stat="normal" onclick="setFull(this, 70)"><i class="fa-solid fa-maximize"></i></div>'+
+						'<div class="time">'+
+						'<span class="current">00:00</span>&nbsp;/&nbsp;<span class="duration">00:00</span>'+
+						'</div></div>'+
+						'<div class="videoContext">'+
+						'<ul>'+
+						'<li onclick="loop(this)"><span class="isActive"></span> <i class="fa-solid fa-arrows-rotate"></i> Loop</li>'+
+						'</ul>'+
+						'</div>': '');
+						d.classList.add('view');
+						d.setAttribute('viewData',b+1);
+						gal[a].replaceChild(d,img[b]);
+				}
+			}
+		}
 	}
 	viewer(s){
 		if(typeof s!=='object'){
@@ -188,6 +282,10 @@ class ImgViewer{
 				s.base.clone = s.base.clone;
 			}
 		}
+		if(s.viewAs!=='image'&&s.viewAs!=='video'){
+			console.error('viewer[1].viewAs must be "image" or "video"');
+			return false;
+		}
 		if(typeof s.animate!=='string'){
 			s.animate = '';
 		}else{
@@ -205,18 +303,18 @@ class ImgViewer{
 				let vwr = document.createElement('div');
 					vwr.classList.add('ImgViewer-viewImg');
 					s.static ? vwr.classList.add('static') : '';
-					s.static ? '' : vwr.setAttribute('onclick','zoomImg(\'out\', this, event);');
+					s.static ? '' : vwr.setAttribute('onclick','zoomImg(\'out\', this, event, false, \''+(s.viewAs==='image' ? 'img': 'video')+'\');');
 					s.animate!=='' ? vwr.classList.add(s.animate) : '';
 					vwr.innerHTML = '<nav><ul class="nav-link">'+
 					(s.base.download ? '<li><a class="img-download" href="#" download="#"><i class="fa-solid fa-download"></i></a></li>' : '')+
 					(s.base.copy ? '<li><a class="img-url-copy" href="#" img-copy-data="" onclick="copyURL(this.getAttribute(\'img-copy-data\'))"><i class="fa-solid fa-link"></i></a></li>' : '')+
 					(s.base.open ? '<li><a class="img-url-open" href="#" img-copy-data="" onclick="window.open(this.getAttribute(\'img-copy-data\'),\'_blank\')"><i class="fa-solid fa-arrow-up-right-from-square"></i></a></li>' : '')+
 					(s.base.clone ? '<li><a class="img-copy" href="#" img-copy-data="" onclick="copyURL(this.getAttribute(\'img-copy-data\'), false)"><i class="fa-solid fa-clone"></i></a></li>' : '')+
-					'<li><span class="ImgViewer-close" onclick="zoomImg(\'out\', this, event, true)"><i class="fa-solid fa-x"></i></span></li></ul></nav><img/><p></p>';
+					'<li><span class="ImgViewer-close" onclick="zoomImg(\'out\', this, event, true, \''+(s.viewAs==='image' ? 'img': 'video')+'\')"><i class="fa-solid fa-x"></i></span></li></ul></nav>'+(s.viewAs==='image' ?'<img/>':'<video></video>')+'<p></p>';
 					document.querySelector('body').appendChild(vwr);
 				let vImg = gal[a].querySelectorAll('div.view');
 				for(let b=0;b<vImg.length;b++){
-					vImg[b].setAttribute('onclick', 'zoomImg("in",this, event);');
+					vImg[b].setAttribute('onclick', 'zoomImg("in",this, event, false, \''+(s.viewAs==='image' ? 'img': 'video')+'\');');
 				}
 			}
 		}
@@ -391,7 +489,8 @@ var resizing = false,
 	dT = 0,
 	dR = 0,
 	dB = 0,
-	dL = 0;
+	dL = 0,
+	vidLoaded = 0;
 function killViews(elem){
 	document.querySelector(elem).remove();
 }
@@ -485,7 +584,7 @@ async function copyURL(data, raw=true){
 		await setToClipboard(blob);
 	}
 }
-function zoomImg(stat, tar, event, force=false){
+function zoomImg(stat, tar, event, force=false, sel='img'){
 	if(resizing||cropping){
 		return false;
 	}
@@ -527,13 +626,24 @@ function zoomImg(stat, tar, event, force=false){
 				vw.style.opacity = 1;
 				vw.style.zIndex = 1;
 				vw.classList.toggle('opened');
-				vw.querySelector('img').src = tar.querySelector('img').src;
-				vw.querySelector('p').innerHTML = '<span>'+new ImgViewer().mkLk(tar.querySelector('img').getAttribute("img-desc"))+'</span>';
-				vw.querySelector('nav a.img-download') ? vw.querySelector('nav a.img-download').href = tar.querySelector('img').src : '';
-				vw.querySelector('nav a.img-url-copy') ? vw.querySelector('nav a.img-url-copy').setAttribute('img-copy-data',tar.querySelector('img').src) : '';
-				vw.querySelector('nav a.img-url-open') ? vw.querySelector('nav a.img-url-open').setAttribute('img-copy-data',tar.querySelector('img').src) : '';
-				vw.querySelector('nav a.img-copy') ? vw.querySelector('nav a.img-copy').setAttribute('img-copy-data',tar.querySelector('img').src) : '';
-				vw.querySelector('nav a') ? vw.querySelector('nav a').download = tar.querySelector('img').alt : '';
+				if(sel==='img'){
+					vw.querySelector('img').src = tar.querySelector('img').src;
+				}
+				if(sel==='video'){
+					vw.querySelector('video').src = tar.querySelector('video').src;
+					vw.querySelector('video').controls = true;
+				}
+				if(sel==='img'){
+					vw.querySelector('p').innerHTML = '<span>'+new ImgViewer().mkNL(new ImgViewer().mkLk(tar.querySelector('img').getAttribute("img-desc")))+'</span>';
+				}
+				if(sel==='video'){
+					vw.querySelector('p').innerHTML = '<span>'+new ImgViewer().mkNL(new ImgViewer().mkLk(tar.querySelector('video').getAttribute("img-desc")))+'</span>';
+				}
+				vw.querySelector('nav a.img-download') ? vw.querySelector('nav a.img-download').href = (sel==='img' ? tar.querySelector('img').src : tar.querySelector('video').src): '';
+				vw.querySelector('nav a.img-url-copy') ? vw.querySelector('nav a.img-url-copy').setAttribute('img-copy-data',(sel==='img' ? tar.querySelector('img').src : tar.querySelector('video').src)) : '';
+				vw.querySelector('nav a.img-url-open') ? vw.querySelector('nav a.img-url-open').setAttribute('img-copy-data',(sel==='img' ? tar.querySelector('img').src : tar.querySelector('video').src)) : '';
+				vw.querySelector('nav a.img-copy') ? vw.querySelector('nav a.img-copy').setAttribute('img-copy-data',(sel==='img' ? tar.querySelector('img').src : tar.querySelector('video').src)) : '';
+				vw.querySelector('nav a') ? vw.querySelector('nav a').download = (sel==='img' ? tar.querySelector('img').alt : tar.querySelector('video').alt) : '';
 			break;
 			case 'out':
 			if(force&&typeof force==='boolean'){
@@ -560,4 +670,144 @@ function zoomImg(stat, tar, event, force=false){
 			break;
 		}
 	
+}
+function calcTime(time){
+	time = Math.round(time);
+	if(time<60){
+		return '00:'+(time<10 ? '0'+time : time);
+	}else if(Math.floor(time/60) < 60){
+		min = Math.floor(time/60);
+		sec = time - min * 60;
+		return (min<10 ? '0'+min : min)+':' + (sec<10 ? '0'+sec : sec);
+	}else{
+		hrs = Math.floor(time/3600);
+		min = time - hrs * 60;
+		sec = time - min * 60;
+		return (hrs<10 ? '0'+hrs : hrs) + ':' + (min<10 ? '0'+min : min) +':' + (sec<10 ? '0'+sec : sec);
+	}
+}
+function addSec(n, s, e){
+	let progress = n.parentElement.querySelector('.video-progress');
+	let key = e.keyCode||e.which;
+	if(key===39){
+		n.currentTime = n.currentTime + parseInt(s);
+	}
+	if(key===37){
+		n.currentTime = n.currentTime - parseInt(s);
+	}
+}
+function actVid(n, e){
+	if(e.keyCode==32||e.which==32||e==32&&n.parentElement.parentElement.parentElement.querySelector('.videoContext').style.display==='none'){
+		let vid = n.parentElement.parentElement.parentElement.querySelector('video');
+		let stat = n.getAttribute('vid-state');
+		let durTime = n.parentElement.parentElement.parentElement.querySelector('.duration');
+		let currTime = n.parentElement.parentElement.parentElement.querySelector('.current');
+		let progress = n.parentElement.parentElement.parentElement.querySelector('.video-progress');
+		let progressFill = n.parentElement.parentElement.parentElement.querySelector('.video-progress-filled');
+		
+		
+		progress.addEventListener('click', (e)=>{
+			const progressTime = (e.offsetX / progress.offsetWidth) * vid.duration;
+			vid.currentTime = progressTime;
+		});
+		vid.addEventListener('timeupdate', function(){
+				currTime.innerHTML = calcTime(vid.currentTime);
+				progressFill.style.width = (vid.currentTime / vid.duration) * 100 + '%';
+				if(((vid.currentTime / vid.duration) * 100) >= 100){
+					vid.pause();
+					n.innerHTML = '<i class="fa-solid fa-play"></i>';
+					n.setAttribute('vid-state','paused');
+				}
+		});
+		switch(stat){
+			case 'paused':
+				n.innerHTML = '<i class="fa-solid fa-pause"></i>';
+				n.setAttribute('vid-state','playing');
+				durTime.innerHTML = calcTime(vid.duration);
+				vid.play();
+				if(vid.readyState===4&&vidLoaded==0){
+					vid.parentElement.querySelector('.loading-container').remove();
+					vidLoaded = 1;
+				}
+			break;
+			case 'playing':
+				n.innerHTML = '<i class="fa-solid fa-play"></i>';
+				n.setAttribute('vid-state','paused');
+				vid.pause();
+			break;
+		}
+	}
+}
+function changeVidVol(n){
+	let vid = n.parentElement.parentElement.parentElement.querySelector('video');
+	let volIcon = n.parentElement.parentElement.parentElement.querySelector('.mute');
+	if(n.value==='0'){
+		volIcon.setAttribute('toggle-stat', 'mute');
+		volIcon.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+	}else{
+		volIcon.innerHTML = '<i class="fa-solid fa-volume"></i>';
+		volIcon.setAttribute('toggle-stat', 'normal');
+	}
+	vid.volume = n.value;
+}
+function setFull(n, e){
+	let base = n.parentElement.parentElement.parentElement;
+	let stat = n.getAttribute('toggle-stat');
+	if(e.keyCode==70||e.which==70||e==70){
+		switch(stat){
+			case 'normal':
+				base.classList.toggle('fullscreen');
+				n.innerHTML = '<i class="fa-solid fa-minimize"></i>';
+				n.setAttribute('toggle-stat','full');
+			break;
+			case 'full':
+				base.classList.toggle('fullscreen');
+				n.innerHTML = '<i class="fa-solid fa-maximize"></i>';
+				n.setAttribute('toggle-stat','normal');
+			break;
+		}
+	}
+}
+function setMute(n, e){
+	let vid = n.parentElement.parentElement.parentElement.querySelector('video');
+	let vol = n.parentElement.parentElement.parentElement.querySelector('.volume');
+	if(e.keyCode===77||e.which===77||e===77){
+		switch(n.getAttribute('toggle-stat')){
+			case 'normal':
+				vid.muted = true;
+				vol.value = '0';
+				n.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+				n.setAttribute('toggle-stat', 'mute');
+			break;
+			case 'mute':
+				vid.muted = false;
+				vol.value = '1';
+				n.innerHTML = '<i class="fa-solid fa-volume"></i>';
+				n.setAttribute('toggle-stat', 'normal');
+			break;
+		}
+	}
+}
+
+function showContent(n, e){
+	let vC = n.parentElement.querySelector('.videoContext');
+	vC.style.display = 'block';
+	vC.style.top = e.clientY+'px';
+	vC.style.left = e.clientX+'px';
+}
+function hideContent(n){
+	let vC = n.parentElement.querySelector('.videoContext');
+	vC.style.display = 'none';
+	
+}
+
+function loop(n){
+	let vid = n.parentElement.parentElement.parentElement.querySelector('video');
+	if(vid.loop){
+		n.querySelector('.isActive').innerHTML = '';
+		vid.loop = false;
+	}else{
+		n.querySelector('.isActive').innerHTML = '<i class="fa-solid fa-check"></i>';
+		vid.loop = true;
+	}
 }
